ebc0f758ff1ef65f583ac9bff9654de9e08ad7cb
leoluizalmeida <55161024+leoluizalmeida@users.noreply.github.com>
2019-09-10 17:08:23 -0300

    Primeiro commit
    Primeiro commit

---
+++ meat-app-starter/src/app/restaurants/restaurants.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core'
+import {Restaurant} from "./restaurant/restaurant.model"
+import {Observable} from 'rxjs/Observable'
+import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
+
+import {MEAT_API} from '../app.api'
+import { Http } from '@angular/http';
+import {ErrorHandler} from '../app.error-handler'
+
+
+@Injectable()
+export class RestaurantsService {
+    constructor(private http: Http){}
+
+    restaurants(): Observable<Restaurant[]>{
+        return this.http.get(`${MEAT_API}/restaurants`)
+          .map(response => response.json())
+          .catch(ErrorHandler.handlerError)
+    }
+}
